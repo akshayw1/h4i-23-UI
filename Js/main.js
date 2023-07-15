1b4905b77512ed19854355b412049097711d10a4
akshayw1 <iec2022117@gmail.com>
2023-07-16 00:17:46 +0530

    Files Uploaded

---
+++ Js/main.js
@@ -0,0 +1,34 @@
+const menu = document.querySelector(".menu li");
+const menuBtn = document.querySelector("#open-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
+
+closeBtn.addEventListener("click", () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+});
+
+var faq = document.getElementsByClassName("faq-page");
+var i;
+
+for (i = 0; i < faq.length; i++) {
+  faq[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var body = this.nextElementSibling;
+    if (body.style.display === "block") {
+      body.style.display = "none";
+    } else {
+      body.style.display = "block";
+    }
+  });
+}
